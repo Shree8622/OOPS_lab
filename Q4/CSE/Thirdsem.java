6a21c6104bdfa277a7acff8d7a7a0c8f7b4affc7
Shree8622 <93328278+Shree8622@users.noreply.github.com>
2022-10-18 14:52:59 +0530

    Add files via upload

---
+++ Q4/CSE/Thirdsem.java
@@ -0,0 +1,8 @@
+package CSE;
+import Library_Session.S1;
+public class Thirdsem {
+    public void Welcomemsg()
+    {
+        System.out.println("Welcome to CSE dept- 3rd sem young budding Engineers");
+    }
+}
